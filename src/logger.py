16b01f9393949c4c4af32d098aece2c0dcf16e97
Sadegh <sadegh15khedry@gmail.com>
2024-07-28 17:44:39 +0330

    tested exception logging using divition by zero

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,10 @@ import logging
 import os
 from datetime import datetime
 
-# LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.log"
-# logs_path = os.path.join('../', 'logs', LOG_FILE)
-# os.mkdir(logs_path)
-# Define the directory and log file name
+
 logs_dir = os.path.join('../', 'logs')
 log_file_name = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 log_file_path = os.path.join(logs_dir, log_file_name)
-
-# LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 # Create the logs directory if it does not exist
 if not os.path.exists(logs_dir):
@@ -23,3 +18,9 @@ logging.basicConfig(
 )
 if __name__ == "__main__":
     logging.info("Logging started.")
+    # try:
+    #     a = 10/0
+        
+    # except:
+    #     # raise CustomException
+    #     logging.error("division by zero")
